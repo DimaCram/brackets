7efb2233579b5b01a6ddb02d58cae53b7e06ea15
Dmitrij <kramkov@ncpi.gov.by>
2020-10-08 17:47:53 +0300

    task check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let pairBrackets = [];
+  str = str.split("");
+
+  bracketsConfig.forEach(element => {
+    pairBrackets.push(element[0] + element[1]);
+  });
+
+  if(str.length % 2 !== 0) return false;
+
+  for(let i = 0; i < str.length; i++){
+
+    if(pairBrackets.indexOf(str[i] + str[i+1]) > -1){
+      str.splice(i, 2);
+      i = -1;
+    }
+  }
+  return str.length === 0;
 }
